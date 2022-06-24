f976f31179d51e3bc9025b0bb7ffcc3a134b4eac
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-06-24 23:08:18 +1000

    Модульные тесты. Добавление плюсов

---
+++ tests/AddPlusesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Keywords\KeywordsGenerator as KG;
+
+/**
+ * Модульные тесты для добавления оператора коротким словам
+ */
+class AddPlusesTest extends TestCase
+{
+    /**
+     * Выполняет простой первый тест
+     */
+    public function testRemove()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::addPluses([
+                "Honda F",
+                "T To Toy"
+            ]), [
+                "Honda +F",
+                "+T +To Toy"
+            ]
+        );
+    }
+
+    /**
+     * Проверяет входные данные в виде пустой строк
+     */
+    public function testEmptyString()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::addPluses([]), []
+        );
+    }
+
+    /**
+     * Проверяет при наличии оператора
+     */
+    public function testPlusExists()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::addPluses([
+                "+Honda +T"
+            ]), [
+                "+Honda +T"
+            ]);
+    }
+}
