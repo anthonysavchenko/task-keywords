1896aa06f58b88fd3949a372c63f46cf3632c306
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-06-24 23:18:16 +1000

    Модульные тесты. Минус-слова

---
+++ tests/ShiftMinusWordsTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Keywords\KeywordsGenerator as KG;
+
+/**
+ * Модульные тесты для перемещение минус-слов в конец фразы
+ */
+class ShiftMinusWordsTest extends TestCase
+{
+    /**
+     * Выполняет простой первый тест
+     */
+    public function testRemove()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::shiftMinusWords([
+                "-Honda Fit",
+                "Toyota -Axio"
+            ]), [
+                "Fit -Honda",
+                "Toyota -Axio"
+            ]
+        );
+    }
+
+    /**
+     * Проверяет входные данные в виде пустой строк
+     */
+    public function testEmptyString()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::shiftMinusWords([]), []
+        );
+    }
+
+    /**
+     * Проверяет только минус-слова
+     */
+    public function testMinusWordsOnly()
+    {
+        $this->assertEqualsCanonicalizing(
+            KG::shiftMinusWords([
+                "-Honda -Fit",
+                "Toyota Axio"
+            ]), [
+                "-Honda -Fit",
+                "Toyota Axio"
+            ]);
+    }
+}
